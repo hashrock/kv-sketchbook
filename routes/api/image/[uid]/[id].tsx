69a320e231945d5cbaf0b5aa8a9fea81da94ba6e
hashrock <hashrock@users.noreply.github.com>
2023-04-30 00:37:37 +0900

    fix

--- routes/api/image/[uid]/[id].tsx
+++ routes/api/image/[uid]/[id].tsx
@@ -2,7 +2,13 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 import { deleteImage, getImage, getUserBySession } from "🛠️/db.ts";
 import { Memo, State, User } from "🛠️/types.ts";
-
+async function remove(
+  uid: string,
+  id: string,
+) {
+  await deleteImage(uid, id);
+  return redirect("/user/" + uid + "");
+}
 export const handler: Handlers<undefined, State> = {
   async GET(req, ctx) {
     const image = await getImage(ctx.params.uid, ctx.params.id);
@@ -26,8 +32,16 @@ export const handler: Handlers<undefined, State> = {
       return new Response("Unauthorized", { status: 401 });
     }
     if (method === "DELETE") {
-      return deleteImage(user.id, ctx.params.id);
+      return remove(user.id, ctx.params.id);
     }
     return new Response("Bad Request", { status: 400 });
   },
 };
+function redirect(location = "/") {
+  const headers = new Headers();
+  headers.set("location", location);
+  return new Response(null, {
+    status: 303,
+    headers,
+  });
+}
