aefa2f5f6828377da07c773de14026804f11324a
stracciatella27 <schoko.bombe44@gmail.com>
2023-05-01 14:54:19 +0200

    realign use flexbox instead

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,8 +2,8 @@ import { AppProps } from "$fresh/server.ts";
 import BrandGithub from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/brand-github.tsx";
 export default function App({ Component }: AppProps) {
   return (
-    <body class="bg-gray-100 min-h-screen grid">
-      <div class="px-4 pt-8 mx-auto w-full max-w-screen-md">
+    <body class="bg-gray-100 min-h-screen flex flex-col">
+      <div class="px-4 pt-8 mx-auto w-full max-w-screen-md flex-1">
         <Component />
       </div>
       <div class="bg-gray-800 mt-24 py-8 text-white text-center flex justify-center items-center gap-2">
